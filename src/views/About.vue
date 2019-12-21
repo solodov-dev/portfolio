539821ace8a3c1a0be2f91bfdeaea1a4b81eebce
Solodov <solodov-dev@gmail.com>
2019-12-22 00:06:22 +0300

    Views updates

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,27 @@
 <template>
   <article>
-    <img src="" alt="Andrey Solodov">
+    <menu-btn></menu-btn>
+    <img src alt="Andrey Solodov" />
     <p>My name is Andrey Solodov. I am a front-end developer</p>
   </article>
 </template>
+
+<script>
+import menuBtn from "@/components/menu-button.vue";
+export default {
+  name: "home",
+  components: {
+    menuBtn: menuBtn
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 article {
   max-width: 30%;
   margin: 0 auto;
+}
+p {
+  font-family: "Montserrat", sans-serif;
 }
 </style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,16 @@
+<template>
+  <menu-btn></menu-btn>
+</template>
+
+<script>
+import menuBtn from "@/components/menu-button.vue";
+export default {
+  name: "home",
+  components: {
+    menuBtn: menuBtn
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
